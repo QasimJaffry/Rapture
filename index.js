4daae442bcc465415799deebf56443e0b535f519
QasimJaffry <qhassan1213@gmail.com>
2023-02-06 02:12:30 +0500

    fixes

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
-/**
- * @format
- */
-
 import App from './src/App';
 import {AppRegistry} from 'react-native';
 import {name as appName} from './app.json';
 
 AppRegistry.registerComponent(appName, () => App);
+
+// "lint-staged": {
+//     "src/**/*.{js,jsx,ts,tsx}": [
+//       "yarn lint",
+//       "yarn format"
+//     ]
+//   }
